63edab5ec4160d55b726d6f4c0b14895f4d146b1
Pablo Mantellini <pmantellini@mulesoft.com>
2014-10-21 16:02:06 -0300

    Fixed typos

--- src/test/java/org/mule/templates/integration/BusinessLogicTestIT.java
+++ src/test/java/org/mule/templates/integration/BusinessLogicTestIT.java
@@ -26,7 +26,7 @@ import com.mulesoft.module.batch.api.BatchManager;
 
 /**
  * The objective of this class is to validate the correct behavior of the flows
- * for this Anypoint Tempalte that make calls to external systems.
+ * for this Anypoint Template that make calls to external systems.
  * 
  */
 public class BusinessLogicTestIT extends AbstractTemplateTestCase {
@@ -53,7 +53,7 @@ public class BusinessLogicTestIT extends AbstractTemplateTestCase {
 	public void testMainFlow() throws Exception {
 		String originalXML = getFileString(TEST_MAT_MASTER_FILE);
 		SapPayloadGenerator generator = new SapPayloadGenerator(originalXML);
-		generator.setTempalteName(TEMPLATE_NAME);
+		generator.setTemplateName(TEMPLATE_NAME);
 		String xmlPayload = generator.generateXML();
 
 		runFlow("callBatchFlow", xmlPayload);
